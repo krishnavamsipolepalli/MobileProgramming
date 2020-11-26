b06cb3a27843045d6b75ae5ad9f4993087d67682
krishnavamsipolepalli <krishnavamsipolepalli97pk@gmail.com>
2020-11-26 01:02:30 -0500

    added movies view page with language filter

--- app/src/main/java/com/example/movie/MoviesList.java
+++ app/src/main/java/com/example/movie/MoviesList.java
@@ -3,16 +3,13 @@ package com.example.movie;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.movie.models.Language;
+import com.example.movie.models.Movies;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -21,47 +18,43 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class MovieLanguage extends AppCompatActivity {
+public class MoviesList extends AppCompatActivity {
 
-    ListView language_list;
+    ListView movie_list;
     FirebaseFirestore database;
-  List<String> languageArray=new ArrayList<String>();
+    String lang="Temp";
+    List<String> MovieArray=new ArrayList<String>();
     ArrayAdapter arraylistadpater;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_language);
-        language_list=findViewById(R.id.LanguageList);
+        setContentView(R.layout.activity_movies_list);
+        movie_list=findViewById(R.id.MovieList);
         database=FirebaseFirestore.getInstance();
+        Bundle bundle = getIntent().getExtras();
+        lang = bundle.getString("selectedItem");
         getData();
-        arraylistadpater=new ArrayAdapter(this,android.R.layout.simple_list_item_1,languageArray);
-        language_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent=new Intent(getApplicationContext(),MoviesList.class);
-                String selectedItem = (String) arraylistadpater.getItem(position);
-                intent.putExtra("selectedItem", selectedItem);
-                startActivity(intent);
-            }
-        });
+        arraylistadpater=new ArrayAdapter(this,android.R.layout.simple_list_item_1,MovieArray);
     }
 
     public void getData(){
 
-        database.collection("language")
+        database.collection("movies")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if(task.isSuccessful()){
                             for(DocumentSnapshot document:task.getResult()){
-                                Language lang=document.toObject(Language.class);
-                                languageArray.add(lang.getLanguageName().toString());
+                                Movies movie=document.toObject(Movies.class);
+                                if(movie.getLanguage().equals(lang))
+                                {
+                                MovieArray.add(movie.getName().toString());
+                                }
                             }
-                            language_list.setAdapter(arraylistadpater);
+                            movie_list.setAdapter(arraylistadpater);
                         }
 
                     }
