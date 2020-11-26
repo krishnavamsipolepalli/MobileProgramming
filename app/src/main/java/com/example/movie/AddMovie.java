555429eed91c2704a933cf5d6938d0a9efeb5e2d
krishnavamsipolepalli <krishnavamsipolepalli97pk@gmail.com>
2020-11-25 21:37:04 -0500

    updated movies model for adding movies to database

--- app/src/main/java/com/example/movie/AddMovie.java
+++ app/src/main/java/com/example/movie/AddMovie.java
@@ -1,11 +1,18 @@
 package com.example.movie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.movie.models.Movies;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddMovie extends AppCompatActivity {
@@ -30,9 +37,36 @@ public class AddMovie extends AppCompatActivity {
         submit=findViewById(R.id.sumitBtn);
 
         database=FirebaseFirestore.getInstance();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=movieName.getText().toString();
+                String rating=movieRating.getText().toString();
+                String genre=movieGenre.getText().toString();
+                String date=movieYear.getText().toString();
+                String language=movieLang.getText().toString();
+                addMovie(name,date,rating,language,genre);
+
+            }
+        });
     }
 
     public void addMovie(String name,String date,String rating,String lang,String genre){
-        database.collection("movies");
+        Movies new_movie=new Movies(name,rating,genre,lang,date);
+        database.collection("movies")
+                .add(new_movie)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(),"Movie Submitted",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),"Error. Try Again"+e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/movie/MainActivity.java
+++ app/src/main/java/com/example/movie/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.movie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button addMovieBtn;
+    Button ViewMovieBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addMovieBtn=findViewById(R.id.add);
+        ViewMovieBtn=findViewById(R.id.View);
+        addMovieBtn.setOnClickListener(this);
+        ViewMovieBtn.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add:
+                Intent intent=new Intent(this.getApplicationContext(), AddMovie.class);
+                this.startActivity(intent);
+                break;
+            case R.id.View:
+                intent=new Intent(this.getApplicationContext(),AddMovie.class);
+                this.startActivity(intent);
+                break;
+
+        }
     }
 }
